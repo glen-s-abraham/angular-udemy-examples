a68daac092f4861e54ffe03acd9841f4ab51d88e
iss211 <Glen.Abraham@immcossi.com>
2023-01-10 13:49:10 +0530

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'admin/users',component:UsersComponent},
+  { path:'admin/rooms',component:RoomsComponent},
+  { path:'calendar',component:CalendarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { UsersComponent } from './admin/users/users.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    UsersComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
